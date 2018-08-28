c9b9e838741d4db8588e489a84e65498068067c0
Douglas Hanley <thesecretaryofwar@gmail.com>
2018-08-28 14:18:35 -0400

    python startup

--- python/00-standard.py
+++ python/00-standard.py
@@ -1,10 +1,21 @@
-def plotter(theme=['scaled']):
+from mectools.hyper import *
+import pandas as pd
+import numpy as np
+
+# patching numpy
+np.concat = np.concatenate
+
+# patcher loader for matplotlib
+def plotter(pyplot=True, theme=['clean'], rc={}):
     import matplotlib as mpl
     import seaborn as sns
+    import pandas as pd
 
     mpl.style.use(theme)
+    mpl.rcParams.update(rc)
     mpl.interactive(True)
 
+    # auto despiner
     def despiner(f):
         def fp(*args, **kwargs):
             dsp = kwargs.pop('despine', True)
@@ -16,3 +27,16 @@ def plotter(theme=['scaled']):
 
     for n in ['plot', 'scatter', 'bar']:
         setattr(mpl.axes.Axes, n, despiner(getattr(mpl.axes.Axes, n)))
+
+    # kill hist grid
+    if not hasattr(pd.Series, 'hist0'):
+        pd.Series.hist0 = pd.Series.hist
+        def hist(*args, **kwargs):
+            if 'grid' not in kwargs:
+                kwargs['grid'] = False
+            pd.Series.hist0(*args, **kwargs)
+        setattr(pd.Series, 'hist', hist)
+
+    if pyplot:
+        import matplotlib.pyplot as plt
+        return plt
