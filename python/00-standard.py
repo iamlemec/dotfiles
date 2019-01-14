c971cea36ec7c3b45c0498eeed96881eab517172
Douglas Hanley <thesecretaryofwar@gmail.com>
2019-01-13 21:52:07 -0500

    no more custom jupyter js

--- python/00-standard.py
+++ python/00-standard.py
@@ -1,30 +1,7 @@
 from mectools.hyper import *
+from mectools.plotter import *
 import pandas as pd
 import numpy as np
 
 # patching numpy
 np.concat = np.concatenate
-
-# patcher loader for matplotlib
-def plotter(pyplot=True, backend='GTK3Agg', theme=['clean'], rc={}):
-    import matplotlib as mpl
-    import seaborn as sns
-    import pandas as pd
-
-    mpl.use(backend)
-    mpl.style.use(theme)
-    mpl.rcParams.update(rc)
-    mpl.interactive(True)
-
-    # kill hist grid
-    if not hasattr(pd.Series, 'hist0'):
-        pd.Series.hist0 = pd.Series.hist
-        def hist(*args, **kwargs):
-            if 'grid' not in kwargs:
-                kwargs['grid'] = False
-            pd.Series.hist0(*args, **kwargs)
-        setattr(pd.Series, 'hist', hist)
-
-    if pyplot:
-        import matplotlib.pyplot as plt
-        return plt
