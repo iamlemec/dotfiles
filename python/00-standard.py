f5d09567a43a4788a8affad2ff6fe7e63582b49d
Douglas Hanley <thesecretaryofwar@gmail.com>
2021-01-17 15:12:10 -0500

    ipython startup

--- python/00-standard.py
+++ python/00-standard.py
@@ -3,8 +3,14 @@ from mectools.plotter import *
 import pandas as pd
 import numpy as np
 
+# build it in
+from itertools import chain
+
 # patching numpy
 np.concat = np.concatenate
+
+# pandas indexing
+idx = pd.IndexSlice
 
 # theme up altair
 altair_config()
